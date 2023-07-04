076f96db76ef7f7666f76f03af0061b1dfdda05d
phrmoy <phrmoy@gmail.com>
2023-07-04 12:19:13 -0300

    fixed Camel case issue

---
+++ src/main/g8/src/main/scala/$package__packaged$/$name__space,Camel$.scala
@@ -0,0 +1,12 @@
+package $package$
+import com.rmet.$base$.$base;format="space,Camel"$.{given, *}
+
+object $name;format="space,Camel"$ {
+  opaque type $name;format="space,Camel"$Type = Object
+  given $name;format="space,Camel"$Type = Object()
+  def validate$name;format="space,Camel"$Imports(using $name;format="space,Camel"$Type) = ()
+
+}
+
+@main def _unsafe$name;format="space,Camel"$Main: Unit =
+  throw RuntimeException(s"$name;format="space,Camel"$ \${Source.filePath} CANNOT BE RUN.")
---
+++ src/main/g8/src/test/scala/$package__packaged$/$name__space,Camel$Test.scala
@@ -0,0 +1,14 @@
+import org.scalatest.funsuite.AnyFunSuite
+// import org.scalatest.flatspec.{*}
+import org.scalatest.matchers.should.{*}
+import $package$.{*}
+import $name;format="space,Camel"$.{given, *}
+import com.rmet.$base$.$base;format="space,Camel"$.{given, *}
+
+class $name;format="space,Camel"$Test extends AnyFunSuite with Matchers {
+  validate$name;format="space,Camel"$Imports
+  validateScbuildImports
+  test("Automated $name;format="space,Camel"$Test") {
+    assert(false)
+  }
+}
